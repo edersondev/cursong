72b13d6069f498b0f3362f819a9fc21cc08bb47a
Ederson Ferreira <ederson.dev@gmail.com>
2017-11-29 09:41:50 -0200

    atualização do menu e menu item

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -7,11 +7,12 @@ import 'rxjs/add/operator/catch';
 import { MEAT_API } from './../app.api';
 import { Restaurant } from './restaurant/restaurant.model';
 import { ErrorHandler } from './../app.error-handler';
+import { MenuItem } from './../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
 
-  constructor(private http: Http){}
+  constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
@@ -29,5 +30,11 @@ export class RestaurantsService {
     return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError)
   }
 }
